a3ac70d558c0b361936d3dafa9a9bf0fe974820d
elleanorquist-prog <elleanor.quist@berkeley.edu>
2025-11-03 10:41:50 -0800

    Update script.js

--- script.js
+++ script.js
@@ -5,27 +5,24 @@ const map = new mapboxgl.Map({
   style: 'mapbox://styles/elleanorquist/cmh9cftwv00pv01sr91551eic',
   center: [-122.27, 37.8], // starting position [lng, lat]. Note that lat must be set between -90 and 90. You can choose what you'd like.
   zoom: 9 // starting zoom, again you can choose the level you'd like.
-    });
+});
 
-map.on('load', function() {
-    map.addSource('points-data', {
-        type: 'geojson',
-        data: 'https://raw.githubusercontent.com/elleanorquist-prog/Berkeleylandmarks/refs/heads/main/data/183data.geojson'
-    });
+map.on('load', function () {
+  map.addSource('points-data', {
+    type: 'geojson',
+    data: 'https://raw.githubusercontent.com/elleanorquist-prog/Berkeleylandmarks/refs/heads/main/data/183data.geojson'
+  });
 
-    map.addLayer({
-        id: 'points-layer',
-        type: 'circle',
-        source: 'points-data',
-        paint: {
-            'circle-color': '#4264FB',
-            'circle-radius': 60,
-            'circle-stroke-width': 2,
-            'circle-stroke-color': '#ffffff'
-        }
-    });
-  map.on('click', 'points-layer', (e) => {
-const coordinates = e.features[0].geometry.coordinates.slice();
-            const properties = e.features[0].properties;
-    });
+  map.addLayer({
+    id: 'points-layer',
+    type: 'circle',
+    source: 'points-data',
+    paint: {
+      'circle-color': '#4264FB',
+      'circle-radius': 6,
+      'circle-stroke-width': 2,
+      'circle-stroke-color': '#ffffff'
+    }
+  });
+ 
 });
